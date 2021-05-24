b9795b701bdcaa854d832d5b9649faa1d618e855
Antra Verma <antraverma99@gmail.com>
2021-05-24 13:17:50 +0530

    fisher shuffle

--- script.js
+++ script.js
@@ -1,39 +1,71 @@
-const overlayText = document.querySelector('.overlay-text')
+const overlay = document.querySelector(".overlay-text");
 const timer = document.querySelector("#timer")
-const endGameModal = document.querySelector("#end-game-modal")
-const replayBtn = document.querySelector("#replay-btn")
+const endGameModal = document.querySelector(".end-game-modal")
+const replayBtn = document.querySelector('.replay-btn')
+const cards = document.querySelectorAll(".card")
 
-const getReady = () => {
-    overlayText.classList.add('visible');
-    overlayText.addEventListener('click', () => {
-        overlayText.classList.remove('visible');
-        startGame(100);
-    })
+//this function is called after loading of the page
+const ready = () =>{
+    overlay.classList.add('visible');
+    const shuffledCards = shuffleArray( Array.from(cards) );
+    console.log(shuffledCards)
 }
 
+//for starting the game 
 const startGame = (totalTime) => {
-    timer.innerText = totalTime;
-  let timeRemaining = totalTime;
-  const timerId = setInterval(() => {
-    timeRemaining--;
-    timer.innerText = timeRemaining;
-    if(timeRemaining === 0) {
-        gameOver()
-        clearInterval(timerId)
+    const timerId = setInterval(() => {            
+        totalTime = totalTime - 1
+        timer.innerText = totalTime
+        if(totalTime === 0){
+            clearInterval(timerId);
+            gameOver()
+        }
+}, 1000)
+}
+
+//function to play the flip sound
+const playFlipSound = () =>{
+    const audio = new Audio('assets/sounds/flip.wav');
+    audio.play()
+}
+
+//to flip the card
+const flipCard = (item) => {
+   item.classList.add('visible')
+}
+
+// Fisher-Yates (aka Knuth) Shuffle
+const shuffleArray = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        let rand = Math.floor(Math.random() * arr.length);
+        [arr[i], arr[rand]] = [arr[rand], arr[i]];
     }
-  }, 1000)
+    return arr;
 }
 
-const gameOver = () =>{
-  endGameModal.classList.add('visible')
-  replayBtn.addEventListener("click", () => {
-      window.location.reload()
-  })
+//when the game ends
+const gameOver = ()=>{
+endGameModal.classList.add('visible');
 }
 
+ready();
 
-if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', getReady());
-} else {
-    getReady();
-}
+
+/*--------------- EVENT LISTENERS ---------------------------*/
+
+overlay.addEventListener('click', () => {
+    overlay.classList.remove('visible');
+    startGame(100)
+})
+
+replayBtn.addEventListener('click', ()=>{
+    window.location.reload()
+})
+
+cards.forEach((card) => {
+    card.addEventListener('click',() =>{
+        playFlipSound();
+        flipCard(card);
+    })
+})
+
